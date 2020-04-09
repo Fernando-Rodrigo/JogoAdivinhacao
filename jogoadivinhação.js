658847b059068dbde9852c4564f3fdc5855c412a
Fernando <fernando22future@gmail.com>
2020-04-08 21:53:41 -0300

    Versão final 1.0

--- jogoadivinhação.js
+++ jogoadivinhação.js
@@ -1,14 +1,14 @@
 var a
+var n = window.document.getElementById('num')
+var r = window.document.getElementById('resultado')
 
 const jogoadivinhação = {
     start() {
-        a = Math.round(100 * Math.random())
-
+        a = Math.round(200 * Math.random())
+        r.innerHTML = ''
     },
 
     chutar() {
-        var n = window.document.getElementById('num')
-        var r = window.document.getElementById('resultado')
         num1 = Number(n.value)
 
         if (num1 == a) {
